1bcbb535f95ae98cdc48f7214cc6d6ca3faf0570
ijborda <imari.borda2018@gmail.com>
2023-09-12 20:41:31 +0800

    Improve pyomo

--- src/models/scheduling_pyomo.py
+++ src/models/scheduling_pyomo.py
@@ -1,7 +1,18 @@
+from typing import List, NamedTuple, cast
 import pandas as pd
 import pyomo.environ as pe
 import pyomo.opt as po
 import csv
+import sys
+
+class Course(NamedTuple):
+    studio: str
+    program: str
+    coach: str
+    day: str
+    time: str
+    start_time: str
+    # end_time: str
 
 ## Define Data
 def get_set(set_name):
@@ -104,7 +115,7 @@ def add_constraint_concurrency(model):
             for d in model.days:
                 lhs = sum(model.x[s, t, d, p, c] for p in model.programs for c in model.coaches)
                 rhs = 1
-                model.concurrency.add(lhs == rhs)
+                model.concurrency.add(lhs <= rhs)
 
 def add_constraint_coach_qualifications(model):
     """
@@ -124,6 +135,92 @@ def add_constraint_coach_qualifications(model):
                 rhs = 0
                 model.coach_qualifications.add(lhs == rhs)
 
+# def add_constraint_program_duration(model):
+#     """
+#     Adds coach qualification constraints. If a coach is unqualified to teach a program, they cannot be assigned to that program.
+
+#     Parameters:
+#     :param model: The model instance
+
+#     Returns:
+#     :return: None
+#     """
+#     model.coach_qualifications = pe.ConstraintList()
+#     for p in model.programs:
+#         for c in model.coaches:
+#             if (model.q[p, c] == 0):
+#                 lhs = sum(model.x[s, t, d, p, c] for s in model.studios for t in model.timeslots for d in model.days)
+#                 rhs = 0
+#                 model.coach_qualifications.add(lhs == rhs)
+
+def convert_to_time(input_value: int):
+    if 0 <= input_value <= 203:
+        hours = 5 + input_value // 12
+        minutes = (input_value % 12) * 5
+        period = "AM" if hours < 12 else "PM"
+        if hours == 12:
+            period = "PM"
+        if hours > 12:
+            hours -= 12
+        return f"{hours:02d}:{minutes:02d} {period}"
+    else:
+        return "Invalid input"
+
+def model_to_array(model) -> List[Course]:
+    res: List[Course] = []
+    # Extract results
+    df = pd.DataFrame(index=pd.MultiIndex.from_tuples(model.x, names=['s', 't', 'd', 'p', 'c']))
+    df['x'] = [pe.value(model.x[key]) for key in df.index]
+    df['p'] = [model.p[key] for key in df.index]
+
+    for (s, t, d, p, c), values in df.iterrows():
+        if (values.x == 1):
+            res.append(Course(cast(str, s), cast(str, p), cast(str, c), cast(str, d), cast(str, t), convert_to_time(int(cast(str, t[1:])))))
+    return res
+
+class AggregateCourse(NamedTuple):
+    studio: str
+    program: str
+    coach: str
+    day: str
+    time_start: str
+    time_end: str
+
+def create_end_times(course: List[Course]) -> List[AggregateCourse]:
+    aggregate_course: List[AggregateCourse] = []
+    
+    course = sorted(course, key=lambda g: (g.studio, g.day, int(g.time[1:])))
+    def transform(c: Course):
+        return AggregateCourse(c.studio, c.program, c.coach, c.day, c.time, c.time)
+    courses_agg = list(map(transform, course))
+
+    current_aggregate = courses_agg[0]
+
+    for next_course in courses_agg[1:]:
+        print(current_aggregate.time_start[1:], next_course.time_start[1:])
+        if (
+            current_aggregate.studio == next_course.studio and
+            current_aggregate.program == next_course.program and
+            current_aggregate.coach == next_course.coach and
+            current_aggregate.day == next_course.day and
+            int(current_aggregate.time_end[1:]) + 1 == int(next_course.time_start[1:])
+        ):
+            current_aggregate = current_aggregate._replace(time_end=next_course.time_start)
+        else:
+            aggregate_course.append(current_aggregate)
+            current_aggregate = next_course
+
+    aggregate_course.append(current_aggregate)
+    
+    return aggregate_course
+
+def pipe_to_output(course: List[AggregateCourse]):
+    course = sorted(course, key=lambda g: (g.studio, g.day, int(g.time_start[1:])))
+    with open('output.out', 'w') as f:
+        sys.stdout = f
+        for g in course:
+            print(f'Studio {g.studio} Day {g.day} -- {g.time_start} to {g.time_end} -- {g.program} -- {g.coach}')
+
 def print_results(model):
     """
     Prints the optimal solution found by the model. For each studio, it prints a timeslot by days table where each value is 'No schedule' if nothing is scheduled or {program}-{coach} to say that coach is scheduled to teach program.
@@ -138,19 +235,24 @@ def print_results(model):
     :return: None
     """
     # Extract results
-    df = pd.DataFrame(index=pd.MultiIndex.from_tuples(model.x, names=['s', 't', 'd', 'p', 'c']))
-    df['x'] = [pe.value(model.x[key]) for key in df.index]
-    df['p'] = [model.p[key] for key in df.index]
+    # df = pd.DataFrame(index=pd.MultiIndex.from_tuples(model.x, names=['s', 't', 'd', 'p', 'c']))
+    # df['x'] = [pe.value(model.x[key]) for key in df.index]
+    # df['p'] = [model.p[key] for key in df.index]
 
     # TO DO: Print should be ascending order
     # Print schedule of each studio (timeslot by day)
-    for studio in model.studios:
-        t_by_d = pd.DataFrame.from_dict({t: {d: 'No schedule' for d in model.days} for t in model.timeslots}, orient='index')
-        for (s, t, d, p, c), values in df.iterrows():
-            if s == studio and values.x == 1:
-                t_by_d.loc[(t, d)] = f'{p}-{c}'
-        print(studio)
-        print(t_by_d.to_markdown())
+    # for studio in model.studios:
+    #     t_by_d = pd.DataFrame.from_dict({t: {d: 'No schedule' for d in model.days} for t in model.timeslots}, orient='index')
+    #     for (s, t, d, p, c), values in df.iterrows():
+    #         if s == studio and values.x == 1:
+    #             t_by_d.loc[(t, d)] = f'{p}-{c}'
+        # Create a list of all 't' values from 't1' to 't204'
+        # all_t_values = [f't{i}' for i in range(1, 205)]
+        # Reindex the DataFrame with all 't' values
+        # t_by_d = t_by_d.reindex(all_t_values, fill_value='')
+        # print(studio)
+        # print(t_by_d.to_markdown())
+    pipe_to_output(create_end_times((model_to_array(model))))
 
 
 def main():
