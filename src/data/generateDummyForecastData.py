d200a30b29d89e38615c5239f795cd9bde8b9c0d
ijborda <imari.borda2018@gmail.com>
2023-09-27 19:06:37 +0800

    Modify dummy data

--- src/data/generateDummyForecastData.py
+++ src/data/generateDummyForecastData.py
@@ -1,6 +1,9 @@
 from typing import List
 import pandas as pd
 from datetime import datetime, timedelta
+import numpy as np
+
+np.random.seed(42)  # For reproducibility
 
 # Generate 2022 dates
 start_date = datetime(2022, 1, 1)
@@ -11,12 +14,23 @@ while current_date <= end_date:
     date_list.append(current_date)
     current_date += timedelta(days=1)
 
+# Lists
+program_list = ['p1', 'p2', 'p3', 'p4']
+coach_list = ['c1', 'c2', 'c3', 'c4']
+studio_list = ['s1', 's2', 's3', 's4']
+
 # Generate capacities
 capacities = [(i ** 2 + 2 * i + 1) for i in range(len(date_list))]
+programs = np.random.choice(program_list, len(date_list))
+coaches = np.random.choice(coach_list, len(date_list))
+studios = np.random.choice(studio_list, len(date_list))
 
 random_data = {
     'date': date_list,
-    'capacity': capacities
+    'capacity': capacities,
+    'program': programs,
+    'coach': coaches,
+    'studio': studios
 }
 
 # Create a DataFrame
